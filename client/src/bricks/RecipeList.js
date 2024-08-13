c07d947b11da5315c863ff7daa61a1edfda5b402
cernyfr1 <fa.cerny@gmail.com>
2024-08-13 15:03:09 +0200

    add Navbar and implement layout changes in RecipeList.js

--- client/src/bricks/RecipeList.js
+++ client/src/bricks/RecipeList.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import Recipe from "./Recipe";
+import React, {useState} from 'react';
+import {Button, Navbar} from "react-bootstrap";
+import Icon from "@mdi/react";
+import {mdiTable, mdiViewGridOutline} from "@mdi/js";
+import RecipeTableList from "./RecipeTableList";
+import RecipeGridList from "./RecipeGridList";
 
 function RecipeList(props) {
-    function getRecipeList(recipeList) {
-        return recipeList.map((recipe) => {
-            return <Recipe key={recipe.id} recipe={recipe} />;
-        })
-    }
 
-    return getRecipeList(props.recipeList);
+    const [viewType, setViewType] = useState("grid");
+    const isGrid = viewType === "grid";
+
+    return (
+        <div>
+            <Navbar bg="light">
+                <div className="container-fluid">
+                    <Navbar.Brand>Seznam receptů</Navbar.Brand>
+                    <Button variant="outline-primary"
+                            onClick={() =>
+                                setViewType((currentState) => {
+                                    if (currentState === "grid") return "table";
+                                    else return "grid";
+                                })
+                            }>
+                        <Icon size={1} path={isGrid ? mdiTable : mdiViewGridOutline} />{" "}
+                        {isGrid ? "Tabulka" : "Grid"}
+                    </Button>
+                </div>
+            </Navbar>
+            {isGrid ? (
+                <RecipeGridList recipeList={props.recipeList} />
+            ) : (
+                <RecipeTableList recipeList={props.recipeList} />
+            )}
+        </div>
+    )
 }
 
 export default RecipeList;
