50e62737155f8b1598b8f9433b638471a930a18b
cernyfr1 <fa.cerny@gmail.com>
2024-08-14 14:38:16 +0200

    implement filtering components and logic

--- client/src/bricks/RecipeList.js
+++ client/src/bricks/RecipeList.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
-import {Button, Navbar} from "react-bootstrap";
+import React, {useMemo, useState} from 'react';
+import {Button, Form, Navbar} from "react-bootstrap";
 import Icon from "@mdi/react";
-import {mdiTable, mdiViewGridOutline} from "@mdi/js";
+import {mdiMagnify, mdiTable, mdiViewGridOutline} from "@mdi/js";
 import RecipeTableList from "./RecipeTableList";
 import RecipeGridList from "./RecipeGridList";
 
@@ -9,28 +9,70 @@ function RecipeList(props) {
 
     const [viewType, setViewType] = useState("grid");
     const isGrid = viewType === "grid";
+    const [searchBy, setSearchBy] = useState("");
+
+    function handleSearch(event) {
+        event.preventDefault();
+        setSearchBy(event.target["searchInput"].value);
+    }
+
+    function handleSearchDelete(event) {
+        if (!event.target.value) setSearchBy("");
+    }
+
+    const filteredRecipeList = useMemo(() => {
+        return props.recipeList.filter((recipe) => {
+            console.log("filtr")
+            return (
+                recipe.name.toLocaleLowerCase().includes(searchBy.toLocaleLowerCase())
+                ||
+                recipe.description.toLocaleLowerCase().includes(searchBy.toLocaleLowerCase())
+            );
+        });
+    }, [searchBy]);
 
     return (
         <div>
             <Navbar bg="light">
                 <div className="container-fluid">
                     <Navbar.Brand>Seznam receptů</Navbar.Brand>
-                    <Button variant="outline-primary"
-                            onClick={() =>
-                                setViewType((currentState) => {
-                                    if (currentState === "grid") return "table";
-                                    else return "grid";
-                                })
-                            }>
-                        <Icon size={1} path={isGrid ? mdiTable : mdiViewGridOutline} />{" "}
-                        {isGrid ? "Tabulka" : "Grid"}
-                    </Button>
+                    <div>
+                        <Form className="d-flex" onSubmit={handleSearch}>
+                            <Form.Control
+                                id={"searchInput"}
+                                style={{ maxWidth: "150px" }}
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                                onChange={handleSearchDelete}
+                            />
+                            <Button
+                                style={{ marginRight: "8px" }}
+                                variant="outline-success"
+                                type="submit"
+                            >
+                                <Icon size={1} path={mdiMagnify} />
+                            </Button>
+                            <Button
+                                variant="outline-primary"
+                                onClick={() =>
+                                    setViewType((currentState) => {
+                                        if (currentState === "grid") return "table";
+                                        else return "grid";
+                                    })
+                                }
+                            >
+                                <Icon size={1} path={isGrid ? mdiTable : mdiViewGridOutline} />{" "}
+                                {isGrid ? "Tabulka" : "Grid"}
+                            </Button>
+                        </Form>
+                    </div>
                 </div>
             </Navbar>
             {isGrid ? (
-                <RecipeGridList recipeList={props.recipeList} />
+                <RecipeGridList recipeList={filteredRecipeList} />
             ) : (
-                <RecipeTableList recipeList={props.recipeList} />
+                <RecipeTableList recipeList={filteredRecipeList} />
             )}
         </div>
     )
